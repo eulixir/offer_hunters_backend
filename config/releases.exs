284699291b61b312f1f8b0972cae8a0c25a21b28
João Pedro Alves <jotalmeida007@hotmail.com>
2021-10-01 17:10:33 -0300

    chore: first deploy

---
+++ config/releases.exs
@@ -0,0 +1,31 @@
+
+import Mix.Config
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :offer_hunters, OfferHunters.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+config :offer_hunters, OfferHuntersWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: secret_key_base
+
+    config :offer_hunters, OfferHuntersWeb.Endpoint, server: true
