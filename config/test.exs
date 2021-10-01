35913fc16c00b011569207a957f57ea99fc07003
João Pedro Alves <jotalmeida007@hotmail.com>
2021-10-01 17:54:08 -0300

    style: format config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -12,11 +12,11 @@ config :offer_hunters, OfferHunters.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-  if System.get_env("GITHUB_ACTIONS") do
-    config :offer_hunters, OfferHunters.Repo,
-      username: "postgres",
-      password: "postgres"
-  end
+if System.get_env("GITHUB_ACTIONS") do
+  config :offer_hunters, OfferHunters.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
