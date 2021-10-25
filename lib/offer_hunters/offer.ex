7bad1c8de12ea61ececd069bb9d04160bcfd5542
João Pedro Alves <jotalmeida007@hotmail.com>
2021-10-25 11:12:49 -0300

    chore: setup initial for database

---
+++ lib/offer_hunters/offer.ex
@@ -0,0 +1,45 @@
+defmodule OfferHunters.Offer do
+  @moduledoc """
+    Create Schema for Offer
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias OfferHunters.User
+
+  @required_params [
+    :product,
+    :description,
+    :promotion_link,
+    :image,
+    :expiration_date,
+    :value,
+    :user_id
+  ]
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @derive {Jason.Encoder, only: [:id] ++ @required_params}
+
+  schema "offers" do
+    field :product, :string
+    field :description, :string
+    field :promotion_link, :string
+    field :image, :string
+    field :expiration_date, :string
+    field :value, :decimal
+
+    belongs_to :user, User, type: :binary_id
+
+    timestamps()
+  end
+
+  def changeset(%{} = attrs) do
+    changeset(%__MODULE__{}, attrs)
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, @required_params)
+    |> validate_required(@required_params)
+    |> unique_constraint(:promotion_link)
+  end
+end
