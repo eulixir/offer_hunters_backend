f05f68882e761a77e2eb646fd6d5dadb2fdf2f5d
João Pedro Alves <jotalmeida007@hotmail.com>
2021-11-01 15:11:54 -0300

    test: create test for offer schema

---
+++ test/offer_hunters/offer_test.exs
@@ -0,0 +1,52 @@
+defmodule OfferHunters.OfferTest do
+  @moduledoc """
+    Tests for offer Schema
+  """
+  use OfferHunters.DataCase, async: true
+
+  import OfferHunters.Factory
+
+  alias Ecto.Changeset
+
+  alias OfferHunters.Offer
+
+  describe "changeset/1" do
+    test "When all params are valid, returns a valid changeset" do
+      response =
+        :offer_params
+        |> build()
+        |> Offer.changeset()
+
+      IO.inspect(response)
+
+      assert %Changeset{
+               changes: %{
+                 description: "Placa de vídeo Galax GeForce RTX 3080 HOF",
+                 expiration_date: "2019-15-10",
+                 image: "src/banana",
+                 promotion_link:
+                   "https =>//www.terabyteshop.com.br/produto/19609/placa-de-video-galax-geforce-rtx-3080-hof-lhr-10gb-gddr6x-dlss-ray-tracing-38nwm3md3bjj?p=714486#banana",
+                 user_id: "a66d859c-b3e4-4482-8460-615d97f91eab"
+               },
+               valid?: true
+             } = response
+    end
+  end
+
+  test "When any fields as empty, returns an error" do
+    response = Offer.changeset(%{})
+
+    assert %Changeset{
+             changes: %{},
+             errors: [
+               description: {"can't be blank", [validation: :required]},
+               promotion_link: {"can't be blank", [validation: :required]},
+               image: {"can't be blank", [validation: :required]},
+               expiration_date: {"can't be blank", [validation: :required]},
+               value: {"can't be blank", [validation: :required]},
+               user_id: {"can't be blank", [validation: :required]}
+             ],
+             valid?: false
+           } = response
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -4,7 +4,7 @@ defmodule OfferHunters.Factory do
   """
   use ExMachina.Ecto, repo: OfferHunters.Repo
 
-  alias OfferHunters.User
+  alias OfferHunters.{Offer, User}
 
   def user_params_factory do
     %{
@@ -19,6 +19,18 @@ defmodule OfferHunters.Factory do
       email: "jp@banana.com",
       name: "jp",
       profile_picture: "src/banana"
+    }
+  end
+
+  def offer_params_factory do
+    %{
+      "expiration_date" => "2019-15-10",
+      "description" => "Placa de vídeo Galax GeForce RTX 3080 HOF",
+      "image" => "src/banana",
+      "promotion_link" =>
+        "https =>//www.terabyteshop.com.br/produto/19609/placa-de-video-galax-geforce-rtx-3080-hof-lhr-10gb-gddr6x-dlss-ray-tracing-38nwm3md3bjj?p=714486#banana",
+      "value" => 10999.0,
+      "user_id" => "a66d859c-b3e4-4482-8460-615d97f91eab"
     }
   end
 end
